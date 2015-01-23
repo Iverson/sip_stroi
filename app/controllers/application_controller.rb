9d6597d67bd5b7c8fdfc0e9b6d8d7376d6e88915
Alexey Krasman <akrasman@gmail.com>
2015-01-23 12:22:41 +0300

    Add defaultHttpAuth gag on production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :require_http_basic_auth if Rails.env == "production"
+
+  def require_http_basic_auth
+    authenticate_or_request_with_http_basic 'Auth' do |name, password|
+      name == 'sip_stroy' && password == 'qwerty'
+    end
+  end
 end
