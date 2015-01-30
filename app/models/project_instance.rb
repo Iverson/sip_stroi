2746693c861f2fb5d5fcd70febb25b46d4e5ebc0
Alexey Krasman <akrasman@gmail.com>
2015-01-30 12:31:41 +0300

    Fix tabs => spaces one more.

--- app/models/project_instance.rb
+++ app/models/project_instance.rb
@@ -1,8 +1,8 @@
 class ProjectInstance < ActiveRecord::Base
-	belongs_to :project
-	belongs_to :instance_type, class_name: 'ProjectInstanceType'
+  belongs_to :project
+  belongs_to :instance_type, class_name: 'ProjectInstanceType'
 
-	validates :instance_type_id, presence: true
-	validates :price, presence: true
-	validates :description, presence: true
+  validates :instance_type_id, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
--- app/models/project_instance_type.rb
+++ app/models/project_instance_type.rb
@@ -1,3 +1,3 @@
 class ProjectInstanceType < ActiveRecord::Base
-	validates :name, presence: true
+  validates :name, presence: true
 end
