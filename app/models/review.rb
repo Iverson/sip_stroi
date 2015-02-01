be1c111900f1b7e315c28055a0fabc0d66945339
Iverson <akrasman@gmail.com>
2015-02-01 18:55:59 +0300

    Fix projects index layout.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :client
 
-  has_attached_file :image, :styles => { :medium => "279x168#" }
+  has_attached_file :image, :styles => { :medium => "279x168#" }, :default_url => "/images/project_nophoto.png"
 
   validates :client_id, presence: true
   validates :text, presence: true
