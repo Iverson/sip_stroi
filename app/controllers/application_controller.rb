2da1850dcca32525dd19baaf1ee4591997d2ae33
Iverson <akrasman@gmail.com>
2015-04-18 22:14:48 +0300

    Remove basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :require_http_basic_auth if Rails.env == "production"
   before_filter :init_feedback
 
   def init_feedback
   	@feedback = ::Feedback.new
     @feedback.build_user_info
-  end
-
-  def require_http_basic_auth
-    authenticate_or_request_with_http_basic 'Auth' do |name, password|
-      name == 'sip_stroy' && password == 'qwerty'
-    end
   end
 end
